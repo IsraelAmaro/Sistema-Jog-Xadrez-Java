b4bf170f2f479cf83ce893070afd0fa415afab2d
Israel Vaz Amaro <devisraelamaro@gmail.com>
2023-01-05 17:18:05 -0300

    Melhor tratamento na ação de promoção de peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,9 +43,15 @@ public class Program {
 				}
 				
 				if(chessMatch.getPromoted() != null) {
+					
 					System.out.println("**************#####*************");
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.next();
+					String type = sc.next().toUpperCase();
+					
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Enter piece for promotion (B/N/R/Q): ");
+						type = sc.next().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 					
 				}
