5842c9c76a5baab89f45a14d5d9e089ee173275a
Israel Vaz Amaro <devisraelamaro@gmail.com>
2023-01-05 17:02:13 -0300

    Jogada especial Promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,14 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("**************#####*************");
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.next();
+					chessMatch.replacePromotedPiece(type);
+					
+				}
+				
 				}
 				catch(ChessException e) {
 					System.out.println(e.getMessage());
